d4a71b34fb336f29db78e0af78d6ed3c3866d8e5
Jones Romão <jones.rom22@gmail.com>
2017-05-11 14:20:37 -0300

    #2 - Adding the buzzer

--- simon_game/simon_game.ino
+++ simon_game/simon_game.ino
@@ -1,46 +1,52 @@
-const int yellow = 2,
-          green = 4,
-          red = 7;
+const int PIN_LED_YELLOW = 2,
+          TONE_YELLOW = 2640,
 
-const int choose_list[] = {yellow, green, red};
+          PIN_LED_GREEN = 4,
+          TONE_GREEN = 2970,
+
+          PIN_LED_RED = 7,
+          TONE_RED = 3300;
+
+const int PIN_BUZZER = 9;
+const int CHOOSE_LIST[] = {PIN_LED_YELLOW, PIN_LED_GREEN, PIN_LED_RED};
+const int LED_TONES[] = {TONE_YELLOW, TONE_GREEN, TONE_RED};
 
 const int HIT = 1, MISSED = 0;
 
 int sequence[100];
 int counter = 1;
 
-void blink(int color, int times=1, int on_time=800, int off_time=400) {
+void blink(int color, int on_time=800, int off_time=500) {
   int test = 0, i;
+  int freq_buzzer = 1000;
+
   for(i = 0; i < 3; i++){
-    if(color == choose_list[i])
+    if(color == CHOOSE_LIST[i]) {
+      freq_buzzer = LED_TONES[i];
       test = 1;
-  }
-  for(int i = 0; i<times; i++){
-    if(test == 1){
-      digitalWrite(color, HIGH);
-      delay(on_time);
-      digitalWrite(color, LOW);
-      delay(off_time);
     }
+  }
+  if(test == 1){
+    digitalWrite(color, HIGH);
+    tone(PIN_BUZZER, freq_buzzer, on_time);
+    delay(on_time);
+    digitalWrite(color, LOW);
+    delay(off_time);
   }
 }
 
 void correct_answer(){
-  digitalWrite(green, HIGH);
-  digitalWrite(yellow, HIGH);
-  delay(500);
-  digitalWrite(green, LOW);
-  digitalWrite(yellow, LOW);
-  delay(500);
+  tone(PIN_BUZZER, 400, 200);
+  delay(400);
+  tone(PIN_BUZZER, 600, 300);
+  delay(1000);
 }
 
 void wrong_answer(){
-  digitalWrite(red, HIGH);
-  digitalWrite(yellow, HIGH);
-  delay(500);
-  digitalWrite(red, LOW);
-  digitalWrite(yellow, LOW);
-  delay(500);
+  tone(PIN_BUZZER, 300, 600);
+  tone(PIN_BUZZER, 100, 300);
+  tone(PIN_BUZZER, 200, 600);
+  delay(1400);
 }
 
 int play(int length) {
@@ -54,12 +60,13 @@ int play(int length) {
   while(keypress_count < length) {
     delay(100);
     for(int i = 0; i < 3; i++) {
-      pins_state[i] = digitalRead(choose_list[i]);
+      pins_state[i] = digitalRead(CHOOSE_LIST[i]);
       if(pins_state[i] == HIGH){
         while(pins_state[i] == HIGH){
-          pins_state[i] = digitalRead(choose_list[i]);
+          pins_state[i] = digitalRead(CHOOSE_LIST[i]);
+          tone(PIN_BUZZER, LED_TONES[i], 100);
         }
-        sequence2check[keypress_count] = choose_list[i];
+        sequence2check[keypress_count] = CHOOSE_LIST[i];
         keypress_count += 1;
         break;
       }
@@ -80,15 +87,15 @@ int play(int length) {
 
 void setup() { 
   Serial.begin(9600);
-  pinMode(yellow, OUTPUT);     
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
+  pinMode(PIN_LED_YELLOW, OUTPUT);     
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
 }
 
 void setup_read() {
-  pinMode(yellow, INPUT);
-  pinMode(red, INPUT);
-  pinMode(green, INPUT);
+  pinMode(PIN_LED_YELLOW, INPUT);
+  pinMode(PIN_LED_RED, INPUT);
+  pinMode(PIN_LED_GREEN, INPUT);
 }
 
 void loop() {
@@ -98,7 +105,7 @@ void loop() {
   //Adds a random blink at the end
   int choice = random(3);
 
-  sequence[counter - 1] = choose_list[choice];
+  sequence[counter - 1] = CHOOSE_LIST[choice];
   
   for(index = 0; index < counter; index++)
     blink(sequence[index]);
